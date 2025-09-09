9dc8bbf8c4bda25de3dfadcb06ba2259ab8919b7
Arseniy Pelevin <arseniy.pelevin@gmail.com>
2025-09-08 22:38:06 -0400

    Generate feedworward and recurrent sparsity masks

--- experiment.py
+++ experiment.py
@@ -31,8 +31,10 @@ class Experiment:
          sessions_key,
          inputs_key,
          input_params_key,
+         ff_mask_key,
+         rec_mask_key,
          params_key,
-         simulation_key) = jax.random.split(key, 6)
+         simulation_key) = jax.random.split(key, 8)
 
         # Pick random number of sessions in this experiment given mean and std
         num_sessions = sample_truncated_normal(
@@ -48,6 +50,16 @@ class Experiment:
             input_params_key,
             cfg["num_inputs"], cfg["num_hidden_pre"],
             input_params_scale=cfg["input_params_scale"]
+        )
+
+        self.feedforward_mask = self.generate_feedforward_mask(
+            ff_mask_key, cfg["num_hidden_pre"], cfg["num_hidden_post"],
+            cfg["feedforward_sparsity"], cfg["postsynaptic_input_sparsity"]
+        )
+
+        self.recurrent_mask = self.generate_recurrent_mask(
+            rec_mask_key, cfg["num_hidden_post"], cfg["recurrent_sparsity"],
+            self.feedforward_mask
         )
 
         # num_hidden_pre -> num_hidden_post plasticity layer
@@ -114,3 +126,99 @@ class Experiment:
 
         return inputs_tensor, jnp.array(step_mask)
 
+    def generate_feedforward_mask(self, key, n_pre, n_post,
+                                  ff_sparsity, input_sparsity):
+        """Generate a binary mask for the feedforward weights to enforce sparsity.
+
+        Args:
+            key: JAX random key.
+            n_pre: Number of presynaptic neurons.
+            n_post: Number of postsynaptic neurons.
+            ff_sparsity [0, 1]: Fraction of nonzero weights in the feedforward layer,
+                of all postsynaptic neurons receiving input (input_sparsity),
+                all presynaptic neurons are guaranteed to have some output:
+                0 - max(n_pre, n_post * input_sparsity) nonzero weights,
+                1 - all presynaptic are connected to all input-receiving postsynaptic.
+            input_sparsity [0, 1]: Fraction of postsynaptic neurons receiving input.
+
+        Returns:
+            A binary mask of shape (n_pre, n_post).
+        """
+        col_key, mask_key, fill_col_key, fill_row_key = jax.random.split(key, 4)
+
+        if not self.cfg.recurrent:
+            input_sparsity = 1.0
+
+        # Choose input postsynaptic neurons (input columns)
+        n_input_post = max(1, int(round(input_sparsity * n_post)))
+        input_cols = jax.random.choice(
+            col_key, n_post, shape=(n_input_post,), replace=False
+        )
+
+        # Generate random mask with given sparsity
+        mask = jnp.zeros((n_pre, n_post))
+        bern = jax.random.bernoulli(mask_key, p=float(ff_sparsity),
+                                    shape=(n_pre, n_input_post)
+                                    ).astype(jnp.float32)
+        mask = mask.at[:, input_cols].set(bern)
+
+        # Ensure no zero rows (presynaptic neurons without output)
+        row_sums = mask[:, input_cols].sum(axis=1)
+        zero_rows = jnp.where(row_sums == 0)[0]
+        chosen_cols = jax.random.choice(
+            fill_col_key, input_cols, shape=zero_rows.shape, replace=True
+        )
+        mask = mask.at[zero_rows, chosen_cols].set(1)
+
+        # Ensure no zero selected columns (input postsynaptic neurons without input)
+        col_sums = mask[:, input_cols].sum(axis=0)
+        zero_cols_idx = jnp.where(col_sums == 0)[0]
+        zero_cols = input_cols[zero_cols_idx]
+        chosen_rows = jax.random.choice(
+            fill_row_key, n_pre, shape=zero_cols.shape, replace=True
+        )
+        mask = mask.at[chosen_rows, zero_cols].set(1)
+
+        return mask
+
+    def generate_recurrent_mask(self, key, n_post, rec_sparsity, ff_mask):
+        """Generate a binary mask for the recurrent weights to enforce sparsity.
+
+        Args:
+            key: JAX random key.
+            n_post: Number of postsynaptic neurons.
+            rec_sparsity [0, 1]: Fraction of nonzero weights in the recurrent layer,
+                all neurons are guaranteed to receive some input and some output:
+                0 - at least one input per neuron,
+                    not counting (allowed) autapses, but counting feedforward input,
+                1 - all-to-all connectivity.
+
+        Returns:
+            A binary mask of shape (n_post, n_post).
+        """
+        mask_key, fill_key = jax.random.split(key)
+
+        # Generate random mask with given sparsity
+        mask = jax.random.bernoulli(mask_key, p=float(rec_sparsity),
+                                    shape=(n_post, n_post)
+                                    ).astype(jnp.float32)
+
+        # Construct test mask to ensure at least one input per neuron
+        # Autapses are not counted as input
+        test_mask = mask.at[jnp.diag_indices(n_post)].set(0)
+        # Feedforward input is counted as input
+        test_mask = jnp.vstack([test_mask, ff_mask.sum(axis=0)])
+
+        # Ensure no zero columns in test mask (postsynaptic neurons without any input)
+        col_sums = test_mask.sum(axis=0)
+        zero_cols = jnp.where(col_sums == 0)[0]
+        chosen_rows = jax.random.choice(
+            fill_key, n_post, shape=zero_cols.shape, replace=True
+        )
+        # If any diagonal elements were chosen, shift them up by one
+        diagonal = jnp.where(zero_cols == chosen_rows)[0]
+        chosen_rows = chosen_rows.at[diagonal].subtract(1)
+
+        mask = mask.at[chosen_rows, zero_cols].set(1)
+
+        return mask
