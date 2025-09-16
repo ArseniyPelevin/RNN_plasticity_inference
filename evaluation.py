3ef72d4129941fec2c8cd17fda0f43aa16bd4d55
Arseniy Pelevin <arseniy.pelevin@gmail.com>
2025-09-16 13:06:52 -0400

    Create evaluation module

---
+++ evaluation.py
@@ -0,0 +1,22 @@
+
+
+def evaluate(key, cfg, params, plasticity_func,
+             test_experiments, expdata):
+    pass
+        
+    # Learn initial weights for test experiments
+
+
+    # Simulate test experiments with learned weights and plasticity
+
+
+    # Evaluate train loss
+
+
+    # Evaluate test loss
+
+
+    # Evaluate percent deviance explained
+
+
+    # Evaluate R2
