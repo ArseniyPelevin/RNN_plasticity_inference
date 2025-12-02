ad21bc6d5fdcb3514bba0f18b31c7b50f1b4ce49
Arseniy Pelevin <arseniy.pelevin@gmail.com>
2025-12-02 19:21:47 +0100

    Direct variables update

--- chaotic_network.py
+++ chaotic_network.py
@@ -15,27 +15,26 @@ def initialize_chaotic_network(N, g=2.0):
 def chaotic_step(x, h, W, q, g=2.0, alpha=0.5, tau_h = 1.0, tau_q=30.0, dt=0.1):
     ''' Single time step of chaotic network with variable balance learning rule '''
     N = len(x)
-    # Update membrane potentials (leaky integrator of inputs)
-    inp = (W @ x) / np.sqrt(N)
-    dh = (-h + inp) / tau_h
+    beta_h = dt / tau_h  # 0.1 / 1.0 = 0.1
+    beta_q = dt / tau_q  # 0.1 / 30.0 = 0.0033
+    beta_W = dt * alpha  # 0.1 * 0.5 = 0.05
 
     # Update running average of second moment of activity
-    dq = (x**2 - q) / tau_q
+    q = (1 - beta_q) * q + beta_q * (x**2)
 
     # Update weights based on deviations from average activity
     M = ((x**2)[:, None] - q[:, None]) + ((x**2)[None, :] - q[None, :])
-    dW = -alpha * M * W
+    dW = - beta_W * M * W
     np.fill_diagonal(dW, 0)  # no autapses
 
-    h = h + dt * dh
-    q = q + dt * dq
-    W = W + dt * dW
+    W = W + dW
 
-    # beta = dt / tau_h
-    # h = (1 - beta) * h + beta * inp
-    # gamma = dt / tau_q
-    # q = (1 - gamma) * q + gamma * (x**2)
+    # Compute network input
+    inp = (W @ x) / np.sqrt(N)
 
+    # Update membrane potentials (leaky integrator of inputs)
+    h = (1 - beta_h) * h + beta_h * inp
+    # Update neuron responses
     x = np.tanh(g*h)
 
     return x, h, W, q
